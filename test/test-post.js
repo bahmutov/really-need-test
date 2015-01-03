d745e355cce83d115801c1c4ddcc2ef04eac5013
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-01-03 09:46:12 -0500

    trying to test pre- and post- functions

--- test/test-post.js
+++ test/test-post.js
@@ -1,7 +1,15 @@
 require = require('really-need');
+var preCalled;
 var foo = require('./foo', {
-    post: function (exported, filename) {
-        return function () { return 'bar'; }
-    }
+  pre: function (source, filename) {
+    console.log('in pre', filename);
+    preCalled = true;
+    return source;
+  },
+  post: function (exported, filename) {
+    console.log('in post', filename);
+    return function () { return 'bar'; };
+  }
 });
-console.assert(foo() === 'bar', 'post worked');
+console.assert(preCalled, 'pre called');
+console.assert(foo() === 'bar', 'post worked ' + foo.toString());
